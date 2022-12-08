82ecc885aec5aef31c4122e5f399ed10c84a2b30
Success96 <lfawas6@gmail.com>
2022-12-08 23:34:07 +0100

    modified program syntax in app.js and home controllers

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
- 
+
 // home route
-const homeRouter = require('./routes/homeRouter');
+const homeRouter = require("./routes/homeRouter");
+const userRouter = require("./routes/userRouter");
 
-app.use('/', homeRouter);
-
+app.use("/", homeRouter);
+app.use("/users", userRouter);
 // starting server
 app.listen(4000, () => {
-    console.log('Server is listening on port 4000.');
-})
+  console.log("Server is listening on port 4000.");
+});
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,4 @@
-const express = require('express');
-
-const homeRouter = express();
-
 // home route
-exports.getHome = homeRouter.get('/', (req, res) => {
-    res.send('Hey there, welcome to exel_comp!');
-})
+exports.getHome = async (req, res) => {
+  res.send("Hey there, welcome to exel_comp!");
+};
