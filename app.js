829902242e89dea6b0d834b04296642711868c16
oabdulhamid <oabdulhamid1@gmail.com>
2022-12-08 23:00:07 +0000

    Added database Schema

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ dotenv.config();
  
 // home route
 const homeRouter = require('./routes/homeRouter');
+const User = require("./models/userModel");
 
 app.use('/', homeRouter);
 
@@ -12,3 +13,8 @@ app.use('/', homeRouter);
 app.listen(4000, () => {
     console.log('Server is listening on port 4000.');
 })
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+    console.log("Connected to db!");
+    app.listen(3000, () => console.log("Server Up and running"));
+});
