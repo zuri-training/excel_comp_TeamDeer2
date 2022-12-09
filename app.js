1a5fcabb59c911150736164810d9c57116eb3f21
Success96 <lfawas6@gmail.com>
2022-12-09 09:33:59 +0100

    updated server setup

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ dotenv.config();
 
 // home route
 const homeRouter = require("./routes/homeRouter");
-const userRouter = require("./routes/userRouter");
 
 app.use("/", homeRouter);
 
-//app.use("/users", userRouter);
 // starting server
 app.listen(4000, () => {
   console.log("Server is listening on port 4000.");
