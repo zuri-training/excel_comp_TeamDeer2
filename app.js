82c8bc9143064690ed5490585c8285afd55274e7
Success96 <lfawas6@gmail.com>
2022-12-09 09:48:26 +0100

    minor changes

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ dotenv.config();
 
 // home route
 const homeRouter = require("./routes/homeRouter");
-const userRouter = require("./routes/userRouter");
+//const userRouter = require("./routes/userRouter");
 
 app.use("/", homeRouter);
-app.use("/users", userRouter);
+//app.use("/users", userRouter);
 // starting server
 app.listen(4000, () => {
   console.log("Server is listening on port 4000.");
