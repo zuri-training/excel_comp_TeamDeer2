4c07084dd705775bee644006518b6c921eff6523
Sally <ayesally1@gmail.com>
2022-12-19 00:03:56 +0100

    modify user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,24 +1,24 @@
-const express = require('express');
+const express = require("express");
 
-const bcrypt = require('bcrypt'); //password handler
+const bcrypt = require("bcrypt"); //password handler
 
-const jwt = require('jsonwebtoken'); //jwt for authentication
+const jwt = require("jsonwebtoken"); //jwt for authentication
 const maxAge = 3 * 24 * 60 * 60;
 
-const nodemailer = require('nodemailer'); //for sending mails
+const nodemailer = require("nodemailer"); //for sending mails
 
 //call mongodb user model
-const User = require('../models/userModel');
+const User = require("../models/userModel");
 
 const userRouter = express();
 
 //function to create token
 const createToken = (id) => {
-  return jwt.sign({ id }, 'excel_comp_teamdeer2');
+  return jwt.sign({ id }, "excel_comp_teamdeer2");
 };
 
 //mailService
-const { trans } = require('./mailService');
+const { trans } = require("./mailService");
 
 //auth middleware for protected routes
 const requireAuth = (req, res, next) => {
@@ -26,11 +26,11 @@ const requireAuth = (req, res, next) => {
   //check jwt exists and verify
 
   if (token) {
-    jwt.verify(token, 'excel_comp_teamdeer2', (err, decodedToken) => {
+    jwt.verify(token, "excel_comp_teamdeer2", (err, decodedToken) => {
       if (err) {
         console.log(err.message);
         res.status(400).json({
-          message: 'Invalid user!',
+          message: "Invalid user!",
         });
       } else {
         console.log(decodedToken);
@@ -40,45 +40,45 @@ const requireAuth = (req, res, next) => {
   } else {
     //redirect user
     res.status(400).json({
-      message: 'You have to be logged in to change your password',
+      message: "You have to be logged in to change your password",
     });
   }
 };
 
-userRouter.post('/signup', (req, res) => {
+userRouter.post("/signup", (req, res) => {
   let { fullName, email, password } = req.body;
   fullName = fullName.trim();
 
   email = email.trim();
   password = password.trim();
 
-  if (fullName == '' || email == '' || password == '') {
+  if (fullName == "" || email == "" || password == "") {
     res.json({
-      status: 'FAILED',
-      message: 'Please fill all the fields',
+      status: "FAILED",
+      message: "Please fill all the fields",
     });
   } else if (!/^[a-zA-Z ]*$/.test(fullName)) {
     res.json({
-      status: 'FAILED',
-      message: 'Invalid name entered',
+      status: "FAILED",
+      message: "Invalid name entered",
     });
   } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
     res.json({
-      status: 'FAILED',
-      message: 'Invalid email entered',
+      status: "FAILED",
+      message: "Invalid email entered",
     });
   } else if (password.length < 6) {
     res.json({
-      status: 'FAILED',
-      message: 'Password must be atleast 6 characters',
+      status: "FAILED",
+      message: "Password must be atleast 6 characters",
     });
   } else {
     User.findOne({ email: req.body.email })
       .then((user) => {
         if (user) {
           res.json({
-            status: 'FAILED',
-            message: 'Email already exists',
+            status: "FAILED",
+            message: "Email already exists",
           });
         } else {
           // Create a new user
@@ -98,23 +98,23 @@ userRouter.post('/signup', (req, res) => {
                 .save()
                 .then((result) => {
                   res.status(200).json({
-                    status: 'SUCCESS',
-                    message: 'User Signup successfully',
+                    status: "SUCCESS",
+                    message: "User Signup successfully",
                     user: result,
                   });
                 })
                 .catch((err) => {
                   res.json({
-                    status: 'FAILED',
-                    message: 'An error occurred while saving user',
+                    status: "FAILED",
+                    message: "An error occurred while saving user",
                   });
                 });
             })
             .catch((err) => {
               console.log(err);
               res.json({
-                status: 'FAILED',
-                message: 'An error occurred while hashing password',
+                status: "FAILED",
+                message: "An error occurred while hashing password",
               });
             });
         }
@@ -122,23 +122,23 @@ userRouter.post('/signup', (req, res) => {
       .catch((err) => {
         console.log(err);
         res.json({
-          status: 'FAILED',
-          message: 'An error occurred while checking for user existence',
+          status: "FAILED",
+          message: "An error occurred while checking for user existence",
         });
       });
   }
 });
 
-userRouter.post('/login', (req, res) => {
+userRouter.post("/login", (req, res) => {
   let { email, password } = req.body;
 
   email = email.trim();
   password = password.trim();
 
-  if (email == '' || password == '') {
+  if (email == "" || password == "") {
     res.json({
-      status: 'FAILED',
-      message: 'Please enter credentials',
+      status: "FAILED",
+      message: "Please enter credentials",
     });
   } else {
     //check user existence
@@ -152,57 +152,57 @@ userRouter.post('/login', (req, res) => {
               if (user) {
                 //send a web token to verify if user is logged in during subsequent request from the same user
                 const token = createToken(data[0]._id);
-                res.cookie('jwt', token, {
+                res.cookie("jwt", token, {
                   httpOnly: true,
                   maxAge: maxAge * 1000,
                 });
 
                 //password matched
                 res.status(200).json({
-                  status: 'SUCCESS',
-                  message: 'Login Successful',
+                  status: "SUCCESS",
+                  message: "Login Successful",
                   user: data,
                 });
               } else {
                 res.status(400).json({
-                  status: 'FAILED',
-                  message: 'Invalid password entered!',
+                  status: "FAILED",
+                  message: "Invalid password entered!",
                 });
               }
             })
             .catch((err) => {
               console.log(err.message);
               res.status(400).json({
-                status: 'FAILED',
-                message: 'An error occurred while comparing password',
+                status: "FAILED",
+                message: "An error occurred while comparing password",
               });
             });
         } else {
           res.status(400).json({
-            status: 'FAILED',
-            message: 'Invalid Credentials entered',
+            status: "FAILED",
+            message: "Invalid Credentials entered",
           });
         }
       })
       .catch((err) => {
         console.log(err);
         res.status(400).json({
-          status: 'FAILED',
-          message: 'An error occurred while comparing credentials',
+          status: "FAILED",
+          message: "An error occurred while comparing credentials",
         });
       });
   }
 });
 
-userRouter.post('/forgot-password', async (req, res) => {
+userRouter.post("/forgot-password", async (req, res) => {
   let { email } = req.body;
 
   email = email.trim();
 
-  if (email == '') {
+  if (email == "") {
     res.status(400).json({
-      status: 'FAILED',
-      message: 'Please enter credentials',
+      status: "FAILED",
+      message: "Please enter credentials",
     });
   }
 
@@ -213,7 +213,7 @@ userRouter.post('/forgot-password', async (req, res) => {
     //check if user exists on our database
     if (!user) {
       res.status(400).json({
-        status: 'FAILED',
+        status: "FAILED",
         message: "User doesn't Exist on our Database",
       });
     }
@@ -223,29 +223,29 @@ userRouter.post('/forgot-password', async (req, res) => {
     //hash token and save as new password for user
     const hashedResetToken = await bcrypt.hash(resetToken.toString(), 10);
 
-    user.password = hashedResetToken;
+    user.passwordResetToken = hashedResetToken;
 
     user
       .save()
       .then((result) => {
         res.status(200).json({
-          status: 'SUCCESS',
-          message: 'Reset Token sent successfully',
+          status: "SUCCESS",
+          message: "Reset Token sent successfully",
           user: result,
         });
       })
       .catch((err) => {
         res.json({
-          status: 'FAILED',
-          message: 'An error occurred while updating password',
+          status: "FAILED",
+          message: "An error occurred while updating password",
         });
       });
 
     //if user exists, send mail to the user with token to set new passWord
     let mailOptions = {
-      from: 'lfawas6@gmail.com',
+      from: "lfawas6@gmail.com",
       to: email,
-      subject: 'Excel_comp Password Reset',
+      subject: "Excel_comp Password Reset",
       text: `This is you Comp_excel reset token ${resetToken}`,
     };
 
@@ -254,13 +254,13 @@ userRouter.post('/forgot-password', async (req, res) => {
   } catch (err) {
     console.log(err.message);
     res.status(400).json({
-      status: 'FAILED',
-      message: 'An error was encountered while trying to send a reset token!',
+      status: "FAILED",
+      message: "An error was encountered while trying to send a reset token!",
     });
   }
 });
 
-userRouter.post('/setNewPassword', async (req, res) => {
+userRouter.post("/setNewPassword", async (req, res) => {
   let { email, token, newPassword, confirmPassword } = req.body;
 
   newPassword = newPassword.trim();
@@ -271,23 +271,23 @@ userRouter.post('/setNewPassword', async (req, res) => {
 
   //validate password and token
   if (
-    email == '' ||
-    token == '' ||
-    newPassword == '' ||
-    confirmPassword == ''
+    email == "" ||
+    token == "" ||
+    newPassword == "" ||
+    confirmPassword == ""
   ) {
     res.json({
-      status: 'FAILED',
-      message: 'Please fill all the fields',
+      status: "FAILED",
+      message: "Please fill all the fields",
     });
   } else if (newPassword.length < 6) {
     res.json({
-      status: 'FAILED',
-      message: 'Password must be atleast 6 characters',
+      status: "FAILED",
+      message: "Password must be atleast 6 characters",
     });
   } else if (confirmPassword !== newPassword) {
     res.status(400).json({
-      message: 'Passwords do not match',
+      message: "Passwords do not match",
     });
   }
 
@@ -299,17 +299,18 @@ userRouter.post('/setNewPassword', async (req, res) => {
 
     if (user.length == 0) {
       res.status(400).json({
-        status: 'FAILED',
-        message: 'Invalid email',
+        status: "FAILED",
+        message: "Invalid email",
       });
     }
 
-    const hashedPassword = user.password;
+    // const hashedPassword = user.password;
+    const dbResetToken = user.passwordResetToken;
 
     //if found and token is valid, update record with new password
 
     bcrypt
-      .compare(token, hashedPassword)
+      .compare(token, dbResetToken)
       .then((data) => {
         if (data) {
           user.password = password;
@@ -318,29 +319,29 @@ userRouter.post('/setNewPassword', async (req, res) => {
             .save()
             .then((result) => {
               res.status(200).json({
-                status: 'SUCCESS',
-                message: 'Password reset successfully',
+                status: "SUCCESS",
+                message: "Password reset successfully",
                 user: result,
               });
             })
             .catch((err) => {
               res.status(400).json({
-                status: 'FAILED',
-                message: 'An error occurred while resetting password',
+                status: "FAILED",
+                message: "An error occurred while resetting password",
               });
             });
         } else {
           //when token is not valid
           res.status(400).json({
-            status: 'FAILED',
-            message: 'Token Invalid',
+            status: "FAILED",
+            message: "Token Invalid",
           });
         }
       })
       .catch((err) => {
         console.log(err.message);
         res.status(400).json({
-          message: 'Invalid Token',
+          message: "Invalid Token",
         });
       });
 
@@ -348,12 +349,12 @@ userRouter.post('/setNewPassword', async (req, res) => {
   } catch (error) {
     console.log(error.message);
     res.status(400).json({
-      message: 'An error was Encountered',
+      message: "An error was Encountered",
     });
   }
 });
 
-userRouter.post('/changePassword', requireAuth, async (req, res) => {
+userRouter.post("/changePassword", requireAuth, async (req, res) => {
   //user must be logged in, hence request must contain jwt in the cookies
   // res.status(500).json({
   //   message: "You are permitted to change your password",
@@ -369,23 +370,23 @@ userRouter.post('/changePassword', requireAuth, async (req, res) => {
 
   //validate user input
   if (
-    email == '' ||
-    oldPassword == '' ||
-    newPassword == '' ||
-    confirmNewPassword == ''
+    email == "" ||
+    oldPassword == "" ||
+    newPassword == "" ||
+    confirmNewPassword == ""
   ) {
     res.status(400).json({
-      status: 'FAILED',
-      message: 'Please enter credentials',
+      status: "FAILED",
+      message: "Please enter credentials",
     });
   } else if (newPassword.length < 6) {
     res.json({
-      status: 'FAILED',
-      message: 'Password must be atleast 6 characters',
+      status: "FAILED",
+      message: "Password must be atleast 6 characters",
     });
   } else if (confirmNewPassword !== newPassword) {
     res.status(400).json({
-      message: 'Passwords do not match',
+      message: "Passwords do not match",
     });
   }
 
@@ -407,37 +408,38 @@ userRouter.post('/changePassword', requireAuth, async (req, res) => {
             .save()
             .then((result) => {
               res.status(200).json({
-                status: 'SUCCESS',
-                message: 'Password Changed successfully',
+                status: "SUCCESS",
+                message: "Password Changed successfully",
                 user: result,
               });
             })
             .catch((err) => {
               res.status(400).json({
-                status: 'FAILED',
-                message: 'An error occurred while changing password',
+                status: "FAILED",
+                message: "An error occurred while changing password",
               });
             });
         } else {
           //when token is not valid
           res.status(400).json({
-            status: 'FAILED',
-            message: 'An error was encountered!',
+            status: "FAILED",
+            message: "An error was encountered!",
           });
         }
       })
       .catch((err) => {
         console.log(err.message);
         res.status(400).json({
-          message: 'Invalid Token',
+          message: "Invalid Token",
         });
       });
   } catch (error) {
     console.log(error.message);
     res.status(400).json({
-      message: 'An error was Encountered',
+      message: "An error was Encountered",
     });
   }
 });
 
-module.exports = { userRouter, requireAuth };
+module.exports = { requireAuth };
+module.exports = userRouter;
