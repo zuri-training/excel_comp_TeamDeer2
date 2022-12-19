4b9d47f6ace9cd43a4c3b1e8fd6ed72541855991
Success96 <lfawas6@gmail.com>
2022-12-19 22:44:09 +0100

    initial commit

--- Backend/controllers/fileController.js
+++ Backend/controllers/fileController.js
@@ -1,5 +1,61 @@
-// file routes
+const multer = require("multer");
 
-exports.upload = async (req, res) => {
-  res.send("Hey there, You can upload your files now!");
+const excelToJson = require("convert-excel-to-json");
+const { request } = require("express");
+
+//call mongodb user model
+//const fileModel = require("./models/fileModel");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+// Upload excel file and import to mongodb
+exports.fileUpload = async (req, res) => {
+  try {
+    upload.single("ExcelTest.xlsx");
+    console.log(req.file);
+    //importExcelData2MongoDB(__dirname + "/uploads/" + req.file.filename);
+
+    res.send("File successfully received");
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({
+      message: error.message,
+    });
+  }
 };
+
+// Import Excel File to MongoDB database
+function importExcelData2MongoDB(filePath) {
+  // -> Read Excel File to Json Data
+  const excelData = excelToJson({
+    sourceFile: filePath,
+    sheets: [
+      {
+        // Header Row -> be skipped and will not be present at our result object.
+        header: {
+          rows: 1,
+        },
+      },
+    ],
+  });
+  // -> Log Excel Data to Console
+  console.log(excelData);
+
+  // Insert Json-Object to MongoDB
+  //   fileModel.insertMany(jsonObj, (err, data) => {
+  //     if (err) {
+  //       console.log(err);
+  //     } else {
+  //       res.redirect("/");
+  //     }
+  //   });
+  //   fs.unlinkSync(filePath);
+}
